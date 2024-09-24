b9c49c3be0ec583b894d713b94a6f0a074907e16
AhmadjonNoyibjonov1 <ahmadjonnoyibjonov6@gmail.com>
2024-09-24 14:38:39 +0300

    fix: loyauts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -8,38 +7,87 @@ import MainLoyaut from "./loyauts/MainLoyaut";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { ThemeProvider } from "./context/ThemeContext";
+import Folder from "./pages/Folder";
+import Message from "./pages/Message";
+import Errorpages from "./pages/Errorpages";
+import Dosc from "./pages/Dosc";
+import Cart from "./pages/Cart"
 
 function App() {
   return (
     <ThemeProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/"
-            element={
-              <MainLoyaut>
-                <Home />
-              </MainLoyaut>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <MainLoyaut>
-                <Profile />
-              </MainLoyaut>
-            }
-          />
-          <Route
-            path="/detailes"
-            element={
-              <MainLoyaut>
-                <Detailes />
-              </MainLoyaut>
-            }
-          />
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <MainLoyaut>
+              <Home />
+            </MainLoyaut>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <MainLoyaut>
+              <Profile />
+            </MainLoyaut>
+          }
+        />
+
+        <Route
+          path="/cart"
+          element={
+            <MainLoyaut>
+              <Cart />
+            </MainLoyaut>
+          }
+        />
+        <Route
+          path="/detailes"
+          element={
+            <MainLoyaut>
+              <Detailes />
+            </MainLoyaut>
+          }
+        />
+
+        <Route
+          path="/dosc"
+          element={
+            <MainLoyaut>
+              <Dosc />
+            </MainLoyaut>
+          }
+        />
+
+        <Route
+          path="/folders"
+          element={
+            <MainLoyaut>
+              <Folder />
+            </MainLoyaut>
+          }
+        />
+
+        <Route
+          path="/message"
+          element={
+            <MainLoyaut>
+              <Message />
+            </MainLoyaut>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <MainLoyaut>
+              <Errorpages />
+            </MainLoyaut>
+          }
+        />
+      </Routes>
     </ThemeProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,50 +43,55 @@ function Header() {
 
       <div className="flex items-center gap-[30px]">
         <label className="swap swap-rotate">
+          {/* hidden checkbox controlling the theme */}
           <input
             type="checkbox"
             onChange={toggleTheme}
             checked={theme === "dark"}
           />
 
+          {/* Custom Sun Icon */}
           <svg
-            className="swap-off h-10 w-10 fill-current"
+            className="swap-off h-8 w-8 fill-current"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
           >
-            <path d="M12 4.75a.75.75 0 0 0 0 1.5h.008a.75.75 0 1 0 0-1.5H12ZM7.757 6.272a.75.75 0 1 0 1.06 1.06L8.82 6.09a.75.75 0 0 0-1.061-1.061l-.002.002ZM5.75 11.992a.75.75 0 0 0 0 1.5H6a.75.75 0 0 0 0-1.5h-.25Zm11.5 0a.75.75 0 0 0 0 1.5h.25a.75.75 0 0 0 0-1.5h-.25Zm-8.482 4.671a.75.75 0 0 0-1.06 1.061l.02.02a.75.75 0 0 0 1.06-1.061l-.02-.02Zm6.707-9.488a.75.75 0 0 0 1.06 1.061l.02-.02a.75.75 0 1 0-1.06-1.061l-.02.02Zm-4.646-.858A4.505 4.505 0 0 0 7.5 12c0 2.485 2.015 4.5 4.5 4.5s4.5-2.015 4.5-4.5S14.485 7.5 12 7.5ZM8.5 12a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0Zm8.057 4.926a.75.75 0 0 0-1.06-1.06l-.02.02a.75.75 0 0 0 1.06 1.061l.02-.02ZM12 18.25a.75.75 0 1 0 0 1.5h.008a.75.75 0 1 0 0-1.5H12Zm-4.243-.682a.75.75 0 0 0-1.061-1.06l-.02.02a.75.75 0 0 0 1.06 1.061l.021-.021Z" />
+            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
 
+          {/* Moon Icon */}
           <svg
-            className="swap-on h-10 w-10 fill-current"
+            className="swap-on h-8 w-8 fill-current"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
           >
-            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05A1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
 
-        <div className="relative">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`h-10 w-10 ${
-              theme === "dark" ? "text-white" : "text-gray-500"
-            }`}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0018 15h-1v-2a4 4 0 10-8 0v2H9a2.032 2.032 0 00-1.595.595L6 17h5m0 0v2a2 2 0 104 0v-2z"
-            />
-          </svg>
-          <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-blue-600 rounded-full">
-            2
-          </span>
-        </div>
+        <Link to="/cart">
+          <div className="relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-10 w-10 ${
+                theme === "dark" ? "text-white" : "text-gray-500"
+              }`}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0018 15h-1v-2a4 4 0 10-8 0v2H9a2.032 2.032 0 00-1.595.595L6 17h5m0 0v2a2 2 0 104 0v-2z"
+              />
+            </svg>
+            <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-blue-600 rounded-full">
+              2
+            </span>
+          </div>
+        </Link>
 
         <Link to="/profile">
           <div className="avatar-group -space-x-6 rtl:space-x-reverse">
